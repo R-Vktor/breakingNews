7894dc7406382d84611f092d7c0ac7fb6bb10c01
Victor alves <viktorw2@gmail.com>
2023-02-05 17:05:43 -0300

    Criacao da estrutura de arquivos e def rotas

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express = require("express");
-const useRoute = require("./src/routes/user.route")
+const userRoute = require("./src/routes/user.route")
 
 const app = express();
 
-app.use("/soma", useRoute)
 
-const port = "localhost:3000"
-app.listen(3000, () => {
-    console.log(`Servidor rodando em ${port}`)
-});
+// e necessário habilitar o 'JSON' pois quando
+ // estamso trabalhando com o API RestFull o padrao é 
+ // receber e enviar informacoes no formato JSON e para 
+ // que o express posa interpretar os dados que chegam 
+ // em JSON é necessario habilitar;
+
+ // tem que habilitar o 'JSON' antes da habilitacao da arota 
+ // pois quando o express começa a ler e configurar ele começa 
+ // de cima para baixo, portanto ele ja temq que ter habilitado o parser 
+ // para JSON antes de internalizar as rotas.
+app.use(express.json()) 
+app.use("/user", userRoute)
+
+
+
+const msg = "localhost:3000";
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em ${msg}`)
+}); 
+
+
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,38 @@
-const soma = (req, res) => {
-    const soma = 100 + 1;
+const create = (req, res) => {
+    
+    // para fins de validacao vamos desmembrar os campos que estao sendo recebidos,
+    // fazemos isso usando o destructuring
+   const {name, username, email, password, avatar, background} = req.body;
 
-    res.send({ soma: soma});
+   if(!name || !username || !email || !password || !avatar || !background) {
+    res.status(400).send({erroMessage: "Submit all fields for resgistration"})
+   }
+
+   // quando vamos enviar respostas fazemos isso em forma de JSON, 
+   // da seguinte forma, ao invés de colocar 'json()' colocamos 
+   // apenas o metodo '.send({})' com a mensagem dentro das chaves, 
+   // dessa forma a nossa resposta é enviada em formato json e
+   //  poderá ser manipulada mais facilmente no 'frontEnd'.
+
+   //É necessário enviar os dados do usuario caso ele 
+   // exista na base de dados para que o front possa 
+   // utilizálos, pois o front nao tem acesso a esse 
+   // tipo de informacao é necessario enviar sempre.
+   // aqui vamo sdevolver um array de objetos.
+   // obviamente nao podemos devolver a informacao da 'SENHA'
+   res.status(201).send({
+       succcessMessage: "Register successfuly!!",
+        user: {
+            name, 
+            username, 
+            email,  
+            avatar, 
+            background,
+        }, 
+    });
+    
 }
 
-module.exports = { soma };
+
+
+module.exports = { create };
